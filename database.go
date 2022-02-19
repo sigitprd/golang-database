3389ba0d2e41e7f16413fecad0e675ba12c6689c
Sigit Priadi <sigitpriadi23@gmail.com>
2022-02-19 22:08:16 +0700

    add file to get connection

---
+++ database.go
@@ -0,0 +1,24 @@
+package golang_database
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetConnection() *sql.DB {
+	// make sql connection
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/golang_database")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
